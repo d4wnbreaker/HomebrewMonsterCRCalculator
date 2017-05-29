dd470e1cd5bbdbb122dc3daf237daee3f3eaec25
Luke <knightfication@gmail.com>
2017-05-29 16:49:07 -0500

    Initial commit
    Let's go

---
+++ Stats.java
@@ -0,0 +1,21 @@
+public class Stats
+{
+    private int challengeRating;
+    private int proficiency;
+    private int armorClass;
+    private int hitPoints;
+    private int attackBonus;
+    private int damagePerRound;
+    private int saveDC;
+
+    public int getProficiency(int newProficiency, int newArmorClass)
+    {
+        proficiency = newProficiency;
+        armorClass = newArmorClass;
+
+        if (proficiency == 2 && armorClass == 13)
+        {
+            challengeRating = 
+        }
+    }
+}
